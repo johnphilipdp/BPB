8ba921494fa23ae24b60453dfb9f06738d69a599
johndp <nhojparungao@gmail.com>
2021-01-29 17:21:27 +0800

    initial commmit

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: '#app',
+  data: {
+    title: 'HEY I AM THE BEST AMONG THE REST',
+    navItems: [
+      'Business <br/> Protection',
+      'Personal <br/> Protection',
+      'Our People',
+      'Blog',
+      'About Us',
+      'Contact',
+    ],
+    toggledNav: false,
+  },
+});
